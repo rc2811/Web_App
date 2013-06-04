5b57f39fa15a03b66d867457af643deba34f05d6
Piotr <pt1411@doc.ic.ac.uk>
2013-06-04 12:54:11 +0100

    First login test working with android app

---
+++ CommandEnum.java
@@ -0,0 +1,3 @@
+public enum CommandEnum {
+	LOGIN, REGISTER
+}
--- ServerFiles/LoginTest/src/com/example/logintest/MainActivity.java
+++ ServerFiles/LoginTest/src/com/example/logintest/MainActivity.java
@@ -2,10 +2,11 @@ package com.example.logintest;
  
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
+
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,7 +17,7 @@ import android.widget.EditText;
 public class MainActivity extends Activity implements OnClickListener {
  
     EditText inputValue=null;
-    Integer doubledValue =0;
+    String doubledValue = null;
     Button doubleMe;
  
     @Override
@@ -40,27 +41,30 @@ public class MainActivity extends Activity implements OnClickListener {
                     public void run() {
  
                         try{
-                            URL url = new URL("http://146.169.53.101:55555/s");
+                            Uri.Builder b = Uri.parse("http://146.169.53.101:55555/s").buildUpon();
+                            String inputString = inputValue.getText().toString();
+                            b.appendQueryParameter("name", inputString);
+                            URL url = new URL(b.build().toString());
                             URLConnection connection = url.openConnection();
  
-                            String inputString = inputValue.getText().toString();
+
                             //inputString = URLEncoder.encode(inputString, "UTF-8");
  
                             Log.d("inputString", inputString);
  
-                            connection.setDoOutput(true);
-                            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
-                            out.write(inputString);
-                            out.close();
+                  //          connection.setDoOutput(true);
+                    //        OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+                    //        out.write(inputString);
+                     //       out.close();
  
                             BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
  
                             String returnString="";
-                            doubledValue =0;
+                    
  
                             while ((returnString = in.readLine()) != null) 
                             {
-                                doubledValue= Integer.parseInt(returnString);
+                                doubledValue= returnString;
                             }
                             in.close();
  
